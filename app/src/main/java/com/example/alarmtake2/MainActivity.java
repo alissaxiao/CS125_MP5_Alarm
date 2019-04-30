57ece72c6610b217f1f495266bc98bd41ea2b575
Jack Xu <JackXJX0616@users.noreply.github.com>
2019-04-29 20:26:57 -0500

    Added toggleButton, linked math page textView buttons

--- app/src/main/java/com/example/alarmtake2/MainActivity.java
+++ app/src/main/java/com/example/alarmtake2/MainActivity.java
@@ -4,21 +4,21 @@ import android.content.Context;
 import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Button;
-import android.widget.Switch;
+import android.widget.CompoundButton;
 import android.widget.TextClock;
 import android.widget.TextView;
 import android.widget.TimePicker;
 import android.content.Intent;
 import android.util.TimeUtils;
 import android.app.AlarmManager;
+import android.widget.ToggleButton;
+import android.app.PendingIntent;
 
 import org.w3c.dom.Text;
 
@@ -34,6 +34,8 @@ import java.util.TimerTask;
 public class MainActivity extends AppCompatActivity {
     TimePicker alarmTime;
     TextClock currentTime;
+    ToggleButton alarmTrigger;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,9 +43,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         alarmTime = findViewById(R.id.alarmTimePicker);
         currentTime = findViewById(R.id.textClock);
-        final Ringtone r = RingtoneManager.getRingtone(getApplicationContext(), RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE));
+        alarmTrigger = findViewById(R.id.alarmTrigger);
+        alarmTrigger.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+
+                }
+            }
+        });
+
         Timer t = new Timer();
         t.scheduleAtFixedRate(new TimerTask() {
+            Ringtone r = RingtoneManager.getRingtone(getApplicationContext(), RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE));
+
             @Override
             public void run() {
                 r.stop();
--- app/src/main/java/com/example/alarmtake2/MathPage.java
+++ app/src/main/java/com/example/alarmtake2/MathPage.java
@@ -1,10 +1,49 @@
 package com.example.alarmtake2;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import java.util.Random;
+import android.widget.Button;
+import android.widget.TextView;
+
 public class MathPage extends AppCompatActivity {
-    @Override
+    Random random = new Random();
+    private int number1 = random.nextInt(100);
+    private int number2 = random.nextInt(100);
+    TextView question;
+    TextView input;
+    TextView clear;
+    TextView sign;
+    TextView enter;
+    TextView seven;
+    TextView eight;
+    TextView nine;
+    TextView four;
+    TextView five;
+    TextView six;
+    TextView one;
+    TextView two;
+    TextView three;
+    TextView zero;
+    TextView decimal;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.math);
+
+        question = findViewById(R.id.equation);
+        input = findViewById(R.id.ret);
+        zero = findViewById(R.id.zero);
+        decimal = findViewById(R.id.decimal);
+        one = findViewById(R.id.one);
+        two = findViewById(R.id.two);
+        three = findViewById(R.id.three);
+        four = findViewById(R.id.four);
+        five = findViewById(R.id.five);
+        six = findViewById(R.id.six);
+        seven = findViewById(R.id.seven);
+        eight = findViewById(R.id.eight);
+        nine = findViewById(R.id.nine);
+        clear = findViewById(R.id.clear);
+        sign = findViewById(R.id.sign);
+        enter = findViewById(R.id.enter);
     }
 }
